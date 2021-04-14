c3a519dc45d412034f29f9d35084fd12a6063406
Diego Leonardo Barbosa <dl-barbosa@hotmail.com>
2021-04-14 15:32:16 -0300

    Projeto Mobile

---
+++ app/src/main/java/com/ads5/appdiegoecommerce/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ads5.appdiegoecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
